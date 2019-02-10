c3976b0dbc5e2bb0834349afa1c1b89a3ecf3e1e
vamsido <vamsikrishnapotti7@gmail.com>
2019-02-10 11:55:30 +0530

    Showing Jobs In Portal Is Done..!

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import job
 # Create your views here.
+
+
+def home(request):
+    newjobs = job.objects.all()
+    return render(request,"jobs/home.html",{"jobs":newjobs})
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
